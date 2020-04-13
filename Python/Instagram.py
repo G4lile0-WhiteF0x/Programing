9b135b7323d3e22891b792b144f84e6c0c9cdb4c
G4lile0-WhiteF0x <57160685+G4lile0-WhiteF0x@users.noreply.github.com>
2020-04-13 00:01:55 -0300

    insta bot

--- Python/Instagram.py
+++ Python/Instagram.py
@@ -2,7 +2,6 @@ from instapy import InstaPy
 
 session = InstaPy(username="v.vicent3", password="VitorVicente26")
 session.login()
-session.set_relationship_bounds(enabled=True, max_followers=1500)
 session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d"], sleepyhead=True, stochastic_flow=True, notify_me=True,
                               peak_likes_hourly=57,
                               peak_likes_daily=585,
@@ -10,6 +9,6 @@ session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d"], 
                                peak_comments_daily=182,)
 
 session.like_by_tags(["balneariocamboriu", "linda", "blogueira", "gata", "lindademais"], amount=40)
-session.set_do_comment(True, percentage=100)
+session.set_do_comment(True, percentage=50)
 session.set_comments(["Lindaaa :heart:", "Gata 🔥🔥", "Q deusa 😍😍"])
 session.set_dont_like(["kids", "crianca", "bebê", "criancas", "nsfw", "nsfw"])
