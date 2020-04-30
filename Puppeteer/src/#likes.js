666ca094fed75735256ecbfd5b9f1b627440b406
G4lile0-WhiteF0x <57160685+G4lile0-WhiteF0x@users.noreply.github.com>
2020-04-29 21:14:04 -0300

    setting my bots

--- Puppeteer/src/#likes.js
+++ Puppeteer/src/#likes.js
@@ -1,63 +1,81 @@
 const puppeteer = require('puppeteer');
 
-
-
+const rash = "user you want";
+const url = 'https://www.instagram.com/explore/tags/' + rash + '/';
+const comment = "Your coment"
 
 (async () => {
-  //page get
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-  await page.goto('https://www.instagram.com/accounts/login/');
-  await page.waitForSelector('input[name="username"]');
+   //variable set
+
+
+     const browser = await puppeteer.launch({headless: false});
+    
+     const page = await browser.newPage();
   
-  //login
-  await page.type('input[name="username"]', 'bot_g4lile0');
-  await page.type('input[name="password"]', 'Insta123');
-  await page.click('button[type="submit"]');
+   //page get
 
- //go to a # 
- await page.waitFor(2000);
- await page.goto('https://www.instagram.com/explore/tags/fofa/');
+     
+     await page.goto('https://www.instagram.com/accounts/login/');
+   
+     await page.waitForSelector('input[name="username"]');
+  
+   //login
+
+    
+     await page.type('input[name="username"]', 'bot_g4lile0');
+    
+     await page.type('input[name="password"]', 'Insta123');
+     
+     await page.click('button[type="submit"]');
+
+   //go to a # 
+
+
+     await page.waitFor(2000);
  
- //photo selection
- await page.click('div.eLAPa')
+     await page.goto();
+  
+   //photo selection
+ 
+     await page.click('div.eLAPa')
 
 
-//pohto like
+   //pohto
 
 
-for(i=0;i<100;i++){
-  var rand = Math.floor(Math.random() * 100) + 1;
-if(rand > 38){
+     for(i=0;i<100;i++){
+        var rand = Math.floor(Math.random() * 100) + 1;
+        if(rand > 38){
 
-  //like
-await page.waitFor(2000);
-await page.click('button.wpO6b');
-};
+         //like
+         await page.waitFor(2000);
+         await page.click('button.wpO6b');
+        };
 
-if(rand > 74){
+        if(rand > 74){
 
-//comment
-  await page.type('textarea.Ypffh', 'Lindaaa❤️❤️');
-  await page.click('button[type="submit"]');
-  await page.waitFor(2000);
-};
+         //comment
+         await page.type('textarea.Ypffh', comment);
+         await page.click('button[type="submit"]');
+         await page.waitFor(2000);
+        };
 
-//follow 
-await page.waitFor(1000);
-await page.click('button.oW_lN');
-
-
+       //follow 
+         await page.waitFor(1000);
+         await page.click('button.oW_lN');
 
 
-//skip
-await page.waitFor(4000);
-await page.click('a._65Bje');
+       //skip
+         await page.waitFor(4000);
+         await page.click('a._65Bje');
 
-};
+      };
 
-page.close()
-})();
+    page.close()
+  }
+)
+
+();
 
 
 
---
+++ Puppeteer/src/proflikes.js
@@ -0,0 +1,98 @@
+const puppeteer = require('puppeteer');
+
+const user = "user you want";
+const url = 'https://www.instagram.com/' + user + '/';
+const comment = "Your coment"
+
+(async() =>{
+        //variable set
+
+
+        const browser = await puppeteer.launch({headless: false});
+
+        const page = await browser.newPage();
+
+        console.log("Have iniciated the Bot")
+
+
+  
+  
+      //page get
+
+      await page.goto('https://www.instagram.com/accounts/login/');
+
+      await page.waitForSelector('input[name="username"]');
+  
+ 
+      //login
+ 
+      await page.type('input[name="username"]', 'login');
+  
+      await page.type('input[name="password"]', 'password');
+ 
+      await page.click('button[type="submit"]');
+
+
+
+
+     //go to a profile
+
+     await page.waitFor(2000);
+
+     await page.goto(url);
+
+     //photo selection
+
+     await page.click('div.eLAPa')
+
+
+
+
+        //pohto 
+
+
+
+     for(i=0;i<100;i++){
+         var rand = Math.floor(Math.random() * 100) + 1;
+  
+           if(rand > 38){
+  
+             //like
+  
+             await page.waitFor(2000);
+            
+             await page.click('span.fr66n button');
+  
+            };
+  
+  
+           if(rand > 74){
+            
+  
+             //comment
+  
+                await page.type('textarea.Ypffh', comment);
+            
+                await page.click('button[type="submit"]');
+            
+                await page.waitFor(2000);
+  
+            };
+  
+  
+         //skip
+  
+            await page.waitFor(4000);
+  
+            await page.click('a._65Bje');
+  
+  
+
+
+        }; 
+
+        page.close()
+    }   
+)
+
+();
