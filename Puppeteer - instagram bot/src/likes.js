ae523d5fce31a97119c436b7f8ddd29cf7546f80
G4lile0-WhiteF0x <57160685+G4lile0-WhiteF0x@users.noreply.github.com>
2020-05-02 19:18:41 -0300

    ready ✔

---
+++ Puppeteer - instagram bot/src/likes.js
@@ -0,0 +1,100 @@
+const puppeteer = require('puppeteer');
+
+const rash = "likeforlikes";
+
+const url = 'https://www.instagram.com/explore/tags/' + rash + '/';
+
+
+const firstname = ['ehyte', 'ragdno', 'nudro' ,'bhe', 'nytra', 'torme' ,'hany','trahy','tonhy','tranhy'];
+
+const secondname = ['fhog','fetus','drago', 'nabc', 'mhoje', 'mage','fadecx', 'sytr','baltazar','gaspar'];
+
+console.log("Have iniciated the Bot");
+
+const math = Math.floor(Math.random() * 10);
+
+const botname = firstname[math] + ' '+ secondname[math];
+
+console.log(botname);
+
+const user = "your user name";
+
+const password = "your password";
+
+
+(async () => {
+   //variable set
+
+     
+     const browser = await puppeteer.launch({headless: false});
+    
+     const page = await browser.newPage();
+
+     console.log("Have iniciated the Bot")
+  
+   //page get
+
+     
+     await page.goto('https://www.instagram.com/accounts/login/');
+   
+     await page.waitForSelector('input[name="username"]');
+  
+   //login
+
+    
+     await page.type('input[name="username"]', user);
+    
+     await page.type('input[name="password"]', password);
+     
+     await page.click('button[type="submit"]');
+
+     console.log("Have loged in")
+  
+  
+     //go to a # 
+
+
+     await page.waitFor(2000);
+ 
+     await page.goto(url);
+  
+   //photo selection
+ 
+     await page.click('div.eLAPa')
+
+
+   //pohto
+
+
+     for(i=0;i<100;i++){
+       
+      var rand = Math.floor(Math.random() * 100) + 1;
+       
+        if(rand > 28){
+
+         //like
+         
+         await page.waitFor(2000);
+         
+         await page.click('button.wpO6b');
+        
+         console.log(botname +  " have liked");
+
+        };
+        
+       //skip
+         
+         await page.waitFor(4000);
+         
+         await page.click('a._65Bje');
+
+      };
+
+    page.close()
+  }
+)
+
+();
+
+
+
