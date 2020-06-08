1efdbc5c02bffb3d620ca38394f2d7ac172beda9
G4lile0-WhiteF0x <57160685+G4lile0-WhiteF0x@users.noreply.github.com>
2020-06-08 11:42:34 -0300

    growing the website

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,29 @@
 const http = require('http');
+const express = require('express');
+const fs = require('fs')
+const app = express();
 
-const hostname = '127.0.0.1';
+
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.get('/', (req, res) => 
+
+
+fs.readFile('index.html', function(error, data){
+ if(error){
+   res.send('Error 404')
+ }else{
+res.write(data)
+
+ }
+
+})
+
+
+
+
+)
+
+app.listen(port, () => console.log('server is on'))
