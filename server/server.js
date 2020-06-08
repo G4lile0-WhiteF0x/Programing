7715b200db9af61fd823449ee28fd0c84c637c6c
G4lile0-WhiteF0x <57160685+G4lile0-WhiteF0x@users.noreply.github.com>
2020-06-08 12:18:34 -0300

    going to make the bots

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const app = express();
 const port = 3000;
 
 
-
 app.get('/', (req, res) => 
 
 
@@ -19,11 +18,49 @@ res.write(data)
 
  }
 
-})
+})),
+
+
+app.get('/fb', (req, res) => 
+
+
+fs.readFile('fb.html', function(error, data){
+ if(error){
+   res.send('Error 404')
+ }else{
+res.write(data)
+
+ }
+
+})),
+
+
+app.get('/tt', (req, res) => 
+
+
+fs.readFile('tt.html', function(error, data){
+ if(error){
+   res.send('Error 404')
+ }else{
+res.write(data)
+
+ }
+
+})),
 
 
 
+app.get('/insta', (req, res) => 
 
-)
+
+fs.readFile('insta.html', function(error, data){
+ if(error){
+   res.send('Error 404')
+ }else{
+res.write(data)
+
+ }
+
+}));
 
 app.listen(port, () => console.log('server is on'))
